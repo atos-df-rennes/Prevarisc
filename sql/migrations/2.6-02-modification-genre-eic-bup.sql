98a1dd23d0c0576389f624b0c2081a0d6b23726e
Maxime Merrien <maxime.merrien@atos.net>
2025-01-16 15:49:18 +0100

    Prend en compte les éventuelles ressources ajoutées par l'utilisateur

--- sql/migrations/2.6-02-modification-genre-eic-bup.sql
+++ sql/migrations/2.6-02-modification-genre-eic-bup.sql
@@ -3,9 +3,5 @@ SET NAMES 'utf8';
 UPDATE `genre` SET `LIBELLE_GENRE` = 'BUP' WHERE `LIBELLE_GENRE` = 'EIC';
 
 UPDATE `resources` 
-SET `name` = 'etablissement_bup_0_0' 
-WHERE `name` = 'etablissement_eic_0_0';
-
-UPDATE `resources`
-SET `text` = 'BUP (Ignorer les groupements - Ignorer la commune)'
-WHERE `name` = 'etablissement_bup_0_0';
+SET `name` = REPLACE(name, 'eic', 'bup'), `text` = REPLACE(text, 'EIC', 'BUP')
+WHERE `name` LIKE 'etablissement_eic%';
